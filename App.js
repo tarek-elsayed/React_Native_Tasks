b62b4b306369e832f4cb6d23b1a9f02cfd84a5c1
tarek-elsayed <tarek.elsayed.morsi@gmail.com>
2021-11-05 01:47:58 +0200

    change config

--- App.js
+++ App.js
@@ -70,14 +70,14 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One 1">
+          <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
           </Section>
-          <Section title="Debug 2">
+          <Section title="Debug">
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
